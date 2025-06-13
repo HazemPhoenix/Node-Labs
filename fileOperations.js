88f0d1e955b5bf19adc7c4eeff8e74433d469dee
hazemphoenix <hazem.mando408@gmail.com>
2025-06-13 18:12:49 +0300

    feat: implement the writeStudentData functions both in the async and the sync way

---
+++ fileOperations.js
@@ -0,0 +1,58 @@
+const fs = require("fs");
+
+const studentData = [
+  {
+    id: 1,
+    name: "Alice Johnson",
+    age: 20,
+    course: "Computer Science",
+    grades: {
+      math: 90,
+      programming: 95,
+    },
+  },
+  {
+    id: 2,
+    name: "Bob Smith",
+    age: 22,
+    course: "Data Science",
+    grades: {
+      statistics: 88,
+      machine_learning: 92,
+    },
+  },
+  {
+    id: 3,
+    name: "Carol Williams",
+    age: 21,
+    course: "Web Development",
+    grades: {
+      html: 95,
+      javascript: 89,
+    },
+  },
+];
+
+// Create the students.json file and add the data to it
+// Async
+function writeStudentDataAsync(data) {
+  try {
+    fs.writeFile("students.json", JSON.stringify(data));
+    console.log("File written successfully");
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+// Sync
+function writeStudentDataSync(data) {
+  try {
+    fs.writeFileSync("students.json", JSON.stringify(data));
+    console.log("File written successfully");
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+// writeStudentDataAsync(studentData);
+// writeStudentDataSync(studentData);
