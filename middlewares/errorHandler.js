2f519cd5f0446aa2fd8fc6eb8aa4cbb25da3e65a
hazemphoenix <hazem.mando408@gmail.com>
2025-06-14 16:05:18 +0300

    feat: implement the errorHandler middleware

---
+++ middlewares/errorHandler.js
@@ -0,0 +1,43 @@
+const AppError = require("../utils/AppError");
+
+const errorHandler = (error, req, res, next) => {
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      status: "Failure",
+      message: error.message,
+    });
+  }
+
+  // Mongoose dublicateKey error
+  if (error.code == 11000) {
+    return res
+      .status(400)
+      .json(
+        errorFormatter(
+          `This ${Object.keys(error.keyValue)[0]} is already taken`
+        )
+      );
+  }
+
+  // Mongoose validation error
+  if (error.name == "ValidationError") {
+    return res.status(400).json(errorFormatter(error.message));
+  }
+
+  // Mongoose cast error
+  if (error.name == "CastError") {
+    return res
+      .status(400)
+      .json(errorFormatter(`Cast Error for ${error.path}: ${error.value}`));
+  }
+
+  // Formatting function to ensure consistency in error structure
+  function errorFormatter(message) {
+    return {
+      status: "Failure",
+      message,
+    };
+  }
+};
+
+module.exports = errorHandler;
